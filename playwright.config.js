c508e27f15519c8ffdc557d5288aa3dd54691452
maydhabhagath-cloud <maydhabhagath@gmail.com>
2026-09-07 18:04:47 +0530

    Remove obsolete frontend test configuration

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-module.exports = {
-  timeout: 30 * 1000,
-  use: {
-    baseURL: 'http://127.0.0.1:8080',
-    headless: true,
-  },
-  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
-};
